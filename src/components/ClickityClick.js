18cf9c0a2c11619aadb9084827234a24167d883a
Oroko <aoroko09@gmail.com>
2021-06-24 11:26:42 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,32 @@
 // Code ClickityClick Component Here
+import React from 'react'
+
+class ClickityClick extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      hasBeenClicked: false,
+      currentTheme: 'blue'
+    }
+  }
+  handleClick = () => {
+    this.setState(
+      (prevState) => (
+        {
+          hasBeenClicked: !prevState.hasBeenClicked,
+        }
+       
+      )
+    );
+    
+  }
+  render(){
+    return(
+      <div>
+      <p>I have {this.state.hasBeenClicked ? null : 'not'} been Clicked </p>
+      <button onClick={this.handleClick}>Click ME</button>
+      </div>
+    )
+  }
+}
+export default ClickityClick
---
+++ src/components/LightSwitch.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import '../index.css'
+
+export class LightSwitch extends Component {
+  constructor(){
+    super()
+    this.state = {
+      toggled: false
+    }
+  }
+  handleClick = () => {
+    this.setState(prevState => ({
+      toggled: !prevState.toggled
+    }))
+  }
+  render() {
+    let light = ''
+    
+    this.state.toggled ? (light += "on") : (light += "off");
+    return (
+      <div>
+        <button className={light} onClick={this.handleClick}>{this.state.toggled ? 'ON': 'OFF' }</button>
+      </div>
+    )
+  }
+}
+
+export default LightSwitch
